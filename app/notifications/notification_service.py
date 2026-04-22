428d301e92f3e2ce6e5cad0be314c3e3c4793037
Brayan Trujillo <inge.brayan075@gmail.com>
2026-04-21 21:49:17 -0500

    refactor(notifications): decouple from FastAPI, add NotificationServiceError

--- app/notifications/notification_service.py
+++ app/notifications/notification_service.py
@@ -1,9 +1,9 @@
-from .notifiers import EmailChannel, WhatsappChannel, SmsChannel
+from .notification_error import NotificationServiceError
 from schemas import PaymentResponse, PaymentData
-from core import dequeue
+from core import INotificationChannel, dequeue
 
-from fastapi import HTTPException, status
 from collections import deque
+from typing import List
 import logging
 
 logger = logging.getLogger(__name__)
@@ -15,29 +15,19 @@ class NotificationService:
     It's responsible for sending notification with information regarding payment details to user.
     Any class that implement this service can define the 'notify_all' method
     """
-    def __init__(
-            self, 
-            email_channel: EmailChannel,
-            whatsapp_channel: WhatsappChannel,
-            sms_channel: SmsChannel
-        ):
-        self.email_channel = email_channel
-        self.whatsapp_channel = whatsapp_channel
-        self.sms_channel = sms_channel  
-    
+    def __init__(self, list_channels: List[INotificationChannel]):
+        self.list_channels = list_channels
+        
     async def notify_all(self, payment_response: PaymentResponse, payment_data: PaymentData):
         """
         Is responsible for notifying the user through all notification channels.
         """
-        queue = deque([(self.email_channel, 0), (self.whatsapp_channel, 0), (self.sms_channel, 0)])
-        notifiers_failed = await dequeue(queue, payment_response)
+        queue = deque([(channel, 0) for channel in self.list_channels])
+        notifiers_failed = await dequeue(queue, payment_response, payment_data)
         if notifiers_failed:
             logger.error(f"Critical failure: The notification service is not responding | Transaction id: {payment_response.transaction_id}")
-            raise HTTPException(
-                status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                detail="Servicio temporalmente inestable. Intente nuevamente mas tarde"
-            )
-            
+            raise NotificationServiceError("Critical failure: notification service unavailable")
+        
         logger.info(
             f"""
                 The user {payment_data.user_data.first_name} {payment_data.user_data.first_surname} was successfully notified of id: {payment_response.transaction_id}
